aad64aca10352df76d3ac8abeb65c490b6ab9f0c
Sourav <souravsaha101@gmail.com>
2021-05-17 22:39:12 +0530

    Clock Done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("HO");
+setInterval(setClock, 1000);
+const hourHand = document.getElementById("hour-hand");
+const minuteHand = document.getElementById("minute-hand");
+const secondHand = document.getElementById("second-hand");
+
+function setClock() {
+  const date = new Date();
+  let seconds = date.getSeconds() / 60;
+  let minute = (seconds + date.getMinutes()) / 60;
+  let hours = (minute + date.getHours()) / 12;
+  clockRotate(secondHand, seconds);
+  clockRotate(minuteHand, minute);
+  clockRotate(hourHand, hours);
+}
+
+const clockRotate = (value, deg) => {
+  value.style.setProperty("--rotation", deg * 360);
+};
+setClock();
